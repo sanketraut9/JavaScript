2879828dcdb007593bbc98ca2b31fd9b432928df
Sanket Raut <90564108+sanketraut9@users.noreply.github.com>
2023-12-12 10:19:09 +0000

    datatypes

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,13 @@
+// Data Type
+// Number => 2 to power 53
+// BigInt 
+// String => ""
+// Boolean => true/ false
+// null => standalon value
+// Symbol => unique
+
+// Object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
