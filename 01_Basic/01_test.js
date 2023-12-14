58d9de4a52546424bc1070d244409283817292b0
“Sanket <sanket.raut0912@gmail.com>
2023-12-15 02:36:03 +0530

    file rename

---
+++ 01_Basic/01_test.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+const accountId = 1234
+let accountEmail = "sanket@gmail.com";
+var accountPass = "sanky123";
+accountCity = "UK"
+let accountState;
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
+
