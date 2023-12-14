6cf98394e99f1a6c87f2251faaeab4db732d7d01
“Sanket <sanket.raut0912@gmail.com>
2023-12-15 03:21:40 +0530

    Conversion of datatypes

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = 33
+
+// console.log(typeof score);    //number
+
+// // let name = 'sam'
+// // console.log(typeof (name));    //string
+
+// // let valueInNumber = Number(name)
+// // console.log(valueInNumber);    //NaN
+// // console.log(typeof valueInNumber);  //number
+
+// let b = false //true //undefined    //null
+
+// let valueInNumber = Number(b)
+// console.log(valueInNumber);   //0 //1 //NaN //0
+// console.log(typeof valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 
+// false -> 0
+
+let isLoggedIn = 1
+ 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 -> true // 0 -> false
+//"sam" => true
+
+let a = 22
+let stringNumber = String(a)
+console.log(stringNumber);       22
+console.log(typeof stringNumber);     //string
