4aeb00aea7a0240126af4bce08c2c8deb41cccab
“Sanket <sanket.raut0912@gmail.com>
2023-12-24 16:00:36 +0530

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,30 @@
+//do, do-while
+
+let i = 0;
+while (i <= 10) {
+    // console.log(`Value of i ${i}`);
+    i = i+2;
+}
+
+
+let myArray = ['Batman', 'Superman', 'Flash', 'Ironman']
+
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`Value of ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+//
+
+let score = 11;
+
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+
+
