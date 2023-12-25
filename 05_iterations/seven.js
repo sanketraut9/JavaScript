83319f89593603bddd2705893d193b6239c32da3
“Sanket <sanket.raut0912@gmail.com>
2023-12-25 22:53:09 +0530

    Map()

---
+++ 05_iterations/seven.js
@@ -0,0 +1,17 @@
+//Maps:
+//map(callbackFn)
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+let newNums =  myNums.map( (num) => num + 10)
+
+// newNums =  myNums.map( (num) => {
+//     return num + 10
+// })
+
+newNums = myNums
+          .map( (num) => num * 2)
+          .map((num) => num + 2)
+          .filter( (num) => num > 10)
+
+console.log(newNums);
